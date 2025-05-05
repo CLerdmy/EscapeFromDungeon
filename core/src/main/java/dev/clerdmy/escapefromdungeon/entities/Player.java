46da439562e70adf324e4cafa59426c994238798
CLerdmy <podturov@gmail.com>
2025-05-05 15:54:56 +0300

    update player

--- core/src/main/java/dev/clerdmy/escapefromdungeon/entities/Player.java
+++ core/src/main/java/dev/clerdmy/escapefromdungeon/entities/Player.java
@@ -1,27 +1,48 @@
 package dev.clerdmy.escapefromdungeon.entities;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import dev.clerdmy.escapefromdungeon.utils.Constants;
 
 public class Player {
-    private float x, y;
-    private Texture texture;
 
-    public Player(float x, float y) {
-        this.x = x;
-        this.y = y;
+    private static final Player instance = new Player();
+    private final Texture texture;
+    private int x;
+    private int y;
+
+    private Player() {
         this.texture = new Texture("player.png");
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public static Player getInstance() {
+        return instance;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(texture, x, y);
+        batch.draw(texture, x, y, Constants.TILE_SIZE, Constants.TILE_SIZE);
     }
 
-    public void move(float dx, float dy) {
-        x += dx;
-        y += dy;
+    public void dispose() {
+        texture.dispose();
     }
 
-    public float getX() { return x; }
-    public float getY() { return y; }
 }
